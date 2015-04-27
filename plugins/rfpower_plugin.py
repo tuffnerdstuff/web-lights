3457adcae9798611eac7c761e99eba5563c474e3
Tuff Nerdstuff <tuffnerdstuff@gmail.com>
2015-04-27 20:20:39 +0000

    rfpower_plugin implemented

--- plugins/rfpower_plugin.py
+++ plugins/rfpower_plugin.py
@@ -1,4 +1,5 @@
 from plugins.server_plugin import ServerPlugin
+import subprocess, os
 
 ATTR_BASE = "base"
 ATTR_UNIT = "unit"
@@ -9,10 +10,16 @@ class RFPowerPlugin(ServerPlugin):
 
     def __init__(self):
         super(RFPowerPlugin,self).__init__()
+        import os
+        path = os.path.abspath(__file__)
+        dir_path = os.path.dirname(path)
+        self.send_path = os.path.join(dir_path,'send')
 
     def set_action(self):
         base,unit,state = self.get_data()
-        print("[%s] base=%i unit=%i state=%s "%(self.name, base, unit, state))
+        print("[%s] base=%s unit=%s state=%s "%(self.name, base, unit, state))
+        subprocess.call(["sudo",self.send_path,base,unit,state])
+        
         
     def get_data(self):
-        return ( int(self.data[ATTR_BASE][0]), int(self.data[ATTR_UNIT][0]), "true" == self.data[ATTR_STATE][0] )
+        return ( self.data[ATTR_BASE][0], self.data[ATTR_UNIT][0], ("1" if "true" == self.data[ATTR_STATE][0] else "0"))
