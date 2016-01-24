47483f68819f1044fb432d13fd503566b28ef91c
Tuff Nerdstuff <tuffnerdstuff@gmail.com>
2016-01-24 15:46:33 +0100

    Fixed rfpower plugin in order to be used with scenes plugin. Refactoring of plugin interface necessary!

--- plugins/rfpower_plugin.py
+++ plugins/rfpower_plugin.py
@@ -16,17 +16,38 @@ class RFPowerPlugin(ServerPlugin):
         self.send_path = os.path.join(dir_path,'send')
 
     def set_state(self,data):
-        # If no units states have been set before, initialize an empty list
-        if not SECT_UNITS in self.data:
-            self.data[SECT_UNITS] = []
-        # Try to find unit state and remove it
-        for unit in self.data[SECT_UNITS]:
-            if unit[ATTR_BASE][0] == data[ATTR_BASE][0] and unit[ATTR_UNIT][0] == data[ATTR_UNIT][0]:
-                # Set unit state
-                unit[ATTR_STATE][0] = data[ATTR_STATE][0]
-                break
+        
+        
+        
+        # TODO: this is bad, change plugin workflow
+        # We set data for all units (e.g. persistence)
+        if SECT_UNITS in data:
+            self.data = data
+        
+        # We set data for one unit
+        else:
+            
+            # If no units states have been set before, initialize an empty list
+            if not SECT_UNITS in self.data:
+                self.data[SECT_UNITS] = []
+            
+            # Try to find unit state and set it
+            for unit in self.data[SECT_UNITS]:
+                if unit[ATTR_BASE][0] == data[ATTR_BASE][0] and unit[ATTR_UNIT][0] == data[ATTR_UNIT][0]:
+                    # Set unit state
+                    unit[ATTR_STATE][0] = data[ATTR_STATE][0]
+                    break
 
     def set_action(self,data):
+        
+        # TODO: set_action should always operate on self.data. Change Interface
+        
+        for unit in self.data[SECT_UNITS]:
+            # switch unit state
+            self.switch_unit(unit);
+
+
+    def switch_unit(self,data):
         base = data[ATTR_BASE][0]
         unit = data[ATTR_UNIT][0]
         state = "1" if "true" == data[ATTR_STATE][0] else "0"
